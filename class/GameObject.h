0f2237da1193e421d9c858884ff9e6f9bca7449f
Blackstar699 <matthieu.mrl69@gmail.com>
2023-11-13 15:57:55 +0100

    GameObject modifications

--- class/GameObject.h
+++ class/GameObject.h
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include "iostream"
 
 class GameObject {
 private:
@@ -7,32 +8,47 @@ private:
     sf::Vector2f size;
     sf::Color color;
     sf::Shape *shape;
+    float speed = 300;
+    bool collision = false;
 
 public:
     //Class Constructors
     GameObject( sf::Vector2f _position, sf::Vector2f _size, sf::Color _color );   //rectangle
     GameObject( sf::Vector2f _position, float _size, sf::Color _color );          //circle
     
-    void draw( sf::RenderWindow &window );
+    //Set Methods
+    void setColor( sf::Color _color );
     
     void setPosition( sf::Vector2f _position );
     
-    sf::Vector2f getPosition();
-    
-    //void setSize( sf::Vector2f _size );
-    
-    sf::Vector2f getSize();
-    
-    void setColor( sf::Color _color );
-    
-    sf::Color getColor();
-    
-    void setRotation( float _rotation );
-    
-    
-    void move( float _speed );
+    void setRotation( float _angle );
     
     void setDirection( sf::Vector2f _direction );
     
+    //Get Methods
+    sf::Color getColor();
+    
+    sf::Vector2f getPosition();
+    
+    sf::Vector2f getSize();
+    
     sf::Vector2f getDirection();
+    
+    //Other Methods
+    void draw( sf::RenderWindow& window );
+    
+    void rotate( float _angle );
+    
+    void move( float deltaTime );
+    
+    //Collisions Methods
+    bool checkCollisions( GameObject& goOther );
+    /*sf::Vector2F GetPosition() { return body.getPosition(); }
+    sf::Vector2f GetHalfSize() { return body.getSize()/ 2.0f; }*/
+    
+    virtual void onCollisionEnter();
+    
+    virtual void onCollisionStay();
+    
+    virtual void onCollisionExit();
 };
--- main.cpp
+++ main.cpp
@@ -1,31 +1,80 @@
 #include "class/GameObject.h"
 
-int main( int argc, char **argv ) {
+int main() {
     
     //Création d'une fenêtre
     const sf::Vector2i window_size(700, 800);
     sf::RenderWindow window(sf::VideoMode(window_size.x, window_size.y), "SFML", sf::Style::Close);
     window.setFramerateLimit(120);
     
-    //Objects Declaration
+    //Objects Declarations
     GameObject rect({200, 200}, {100, 50}, sf::Color::Red);
     GameObject circle({100, 100}, 50, sf::Color::Green);
+    
+    sf::Clock clock;
+    sf::Time time;
+    float deltaTime = 0;
+    
     
     //GameLoop
     while ( window.isOpen() ) {
         //EVENT
         sf::Event o_event{};
+        
+        //Poll for Events
         while ( window.pollEvent(o_event) ) {
             if ( o_event.type == sf::Event::KeyPressed ) {
-                rect.setRotation(45);
+                if(o_event.key.code == sf::Keyboard::R){
+                    rect.rotate(45);
+                }
+                if(o_event.key.code == sf::Keyboard::Z){
+                    rect.setDirection({rect.getDirection().x,-1});
+                    rect.move(deltaTime);
+                }
+                if(o_event.key.code == sf::Keyboard::Q){
+                    rect.setDirection({-1,rect.getDirection().y});
+                    rect.move(deltaTime);
+                }
+                if(o_event.key.code == sf::Keyboard::S){
+                    rect.setDirection({rect.getDirection().x,1});
+                    rect.move(deltaTime);
+                }
+                if(o_event.key.code == sf::Keyboard::D){
+                    rect.setDirection({1,rect.getDirection().y});
+                    rect.move(deltaTime);
+                }
             }
             
+            if (o_event.type == sf::Event::KeyReleased) {
+                if(o_event.key.code == sf::Keyboard::Z){
+                    rect.setDirection({rect.getDirection().x,0});
+                }
+                if(o_event.key.code == sf::Keyboard::Q){
+                    rect.setDirection({0,rect.getDirection().y});
+                }
+                if(o_event.key.code == sf::Keyboard::S){
+                    rect.setDirection({rect.getDirection().x,0});
+                }
+                if(o_event.key.code == sf::Keyboard::D){
+                    rect.setDirection({0,rect.getDirection().y});
+                }
+            }
             
-            if ( o_event.type == sf::Event::Closed )
+            if(o_event.type == sf::Event::MouseMoved){
+                sf::Vector2i mouse = sf::Mouse::getPosition(window);
+                float angle;
+                angle = -atan2(mouse.x - rect.getPosition().x, mouse.y - rect.getPosition().y) * 180 / 3.14159;
+                rect.setRotation(angle);
+            }
+            
+            if ( o_event.type == sf::Event::Closed ) {
                 window.close();
+            }
         }
         
         //UPDATE
+        time        = clock.restart();
+        deltaTime   = time.asSeconds();
         
         //DRAW
         window.clear();
